c3a38e8a1200956c543bf0a6062ae6402cd80922
Curtis <curtisodell1@gmail.com>
2024-01-19 18:27:49 -0700

    working stripe add in

--- server/server.js
+++ server/server.js
@@ -35,8 +35,8 @@ app.post("/create-checkout-session", async (req, res) => {
           quantity: item.quantity,
         }
       }),
-      success_url: `${process.env.CLIENT_URL}/success.html`,
-      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+      success_url: `${process.env.SERVER_URL}/success.html`,
+      cancel_url: `${process.env.SERVER_URL}/cancel.html`,
     })
     res.json({ url: session.url })
   } catch (e) {
